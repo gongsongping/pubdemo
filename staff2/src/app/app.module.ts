b665c88d7fed72aaf17295b14980bc704caad71a
jiangwenwu <359580446@qq.com>
2017-03-08 17:39:29 +0800

    home新页面提交

--- staff2/src/app/app.module.ts
+++ staff2/src/app/app.module.ts
@@ -4,11 +4,8 @@ import { MyApp } from './app.component';
 import { Home } from '../pages/home/home';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
-<<<<<<< HEAD
-=======
-import { Home } from '../pages/home/home';
+
 import { LoginModal, NestedContent } from '../providers/services';
->>>>>>> 7f43978c00f3c17ce0409c89e5384252c61f013b
 
 
 @NgModule({
