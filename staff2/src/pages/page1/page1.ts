94780ec93b156bbecfc8b5e582b4fe7fb19e9cd2
jiangwenwu <359580446@qq.com>
2017-03-09 09:39:50 +0800

    冲突提交

--- staff2/src/pages/page1/page1.ts
+++ staff2/src/pages/page1/page1.ts
@@ -1,23 +1,11 @@
 import {Component} from '@angular/core';
 
-<<<<<<< HEAD
+
 import { NavController} from 'ionic-angular';
 
-
-@Component({
-  selector: 'page-page1',
-  templateUrl: 'page1.html'
-})
-export class Page1 {
-  
-  constructor(public navCtrl: NavController) {
-    
-  }
-  
-=======
-import {NavController} from 'ionic-angular';
-// import {ViewController} from 'ionic-angular';
 import { Home } from '../home/home';
+
+
 
 
 @Component({selector: 'page-page1', templateUrl: 'page1.html'})
@@ -30,5 +18,4 @@ export class Page1 {
   ngOnInit() {
     console.log('----page1---- Page oninit');
   }
->>>>>>> 7f43978c00f3c17ce0409c89e5384252c61f013b
 }
