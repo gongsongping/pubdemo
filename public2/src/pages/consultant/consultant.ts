c7e795f91eeabc31b6f14433addcab21a6b9f246
binh <binh@binhs-MacBook-Pro.local>
2017-03-23 09:56:53 +0800

    slidestest

--- public2/src/pages/consultant/consultant.ts
+++ public2/src/pages/consultant/consultant.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams , Slides} from 'ionic-angular';
 
 /*
   Generated class for the Consultant page.
@@ -12,15 +12,42 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'consultant.html'
 })
 export class Consultant {
-
+  @ViewChild(Slides) slds:Slides
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ConsultantPage');
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter ConsultantPage');
+    // this.slds.slideTo(2)
+    console.log('---',this.slds.getActiveIndex(),'---');
   }
   tabs = ['置业顾问','房管家']
-  activeTab = 0
+  activeTab
   chooseTab (i){
+    console.log('---',i,'---',this.slds.getActiveIndex());
     this.activeTab = i
+    this.slds.slideTo(i)
+    console.log('---',i,this.slds.getActiveIndex(),'---');
+  }
+  
+  ionSlideDrag(e){
+    console.log('ionSlideDrag',this.slds.getActiveIndex(),e.swipeDirection,e.pageX,e.pageY);
+  }
+  ionSlideWillChange(e){
+    console.log('ionSlideWillChange',this.slds.getActiveIndex(),e.swipeDirection,e.pageX,e.pageY);
+  }
+  ionSlideDidChange(e){
+    console.log('ionSlideDidChange',this.slds.getActiveIndex(),e.swipeDirection,e.pageX,e.pageY);
+  }
+  ionSlidePrevStart(){
+    console.log('ionSlidePrevStart',this.slds.getActiveIndex());
+  }
+  ionSlidePrevEnd(){
+    console.log('ionSlidePrevEnd',this.slds.getActiveIndex());
+  }
+  ionSlideNextStart(){
+    console.log('ionSlideNextStart',this.slds.getActiveIndex());
+  }
+  ionSlideNextEnd(){
+    console.log('ionSlideNextEnd',this.slds.getActiveIndex());
   }
 }
