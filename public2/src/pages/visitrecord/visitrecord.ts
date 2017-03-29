ef6cb3842d4f51a91b8513835f70ab2ed69ff737
jiangwenwu <359580446@qq.com>
2017-03-29 18:31:34 +0800

    看房记录

--- public2/src/pages/visitrecord/visitrecord.ts
+++ public2/src/pages/visitrecord/visitrecord.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import axios from 'axios';
 /*
   Generated class for the Visitrecord page.
 
@@ -8,15 +9,49 @@ import { NavController, NavParams } from 'ionic-angular';
   Ionic pages and navigation.
 */
 @Component({
-  selector: 'page-visitrecord',
-  templateUrl: 'visitrecord.html'
+    selector: 'page-visitrecord',
+    templateUrl: 'visitrecord.html'
 })
 export class Visitrecord {
+    selectTab = 1;
+    userInfo:any;
+    total:any;
+    record:any;
+    visit = 1;
+    statusParams='&statusIn=[-2,-1,0,1,3,4,5,8,9]';
+    constructor(public navCtrl: NavController, public navParams: NavParams) { }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+    records = [{ name: '全部', status: '[-2,-1,0,1,3,4,5,8,9]',id:1 }, { name: '看房中', status: 3 ,id:2}, { name: '待付款', status: 1 ,id:3}, { name: '过户中', status: '[5,8,9]' ,id:4}, { name: '已完成', status: 0 ,id:5}]
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad VisitrecordPage');
-  }
-
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad SellrecordPage');
+        if (localStorage.getItem('userInfo')) {
+            this.userInfo = JSON.parse(localStorage.getItem('userInfo'));
+            console.log('-----', this.userInfo);
+        } else {
+            this.userInfo = ''
+        }
+    }
+    ionViewWillEnter() {
+        this.initial();
+    }
+    initial(){
+        let vm = this;
+        let url = '/api/mission/missions?typeIn=[1,2]&demanderId=' + vm.userInfo.id + vm.statusParams;
+        axios.get(url)
+            .then(function (res) {
+                console.log(res)
+                vm.total = res.data.total
+                vm.record = res.data.data;
+            })
+    }
+    selectStatus(m){
+        let vm = this;
+        vm.selectTab = m.id;
+        vm.statusParams = '&statusIn=' + m.status;
+        vm.initial();
+    }
+    statisticsNone(){
+        this.visit = 2;
+    }
 }
--- public2/src/providers/services.ts
+++ public2/src/providers/services.ts
@@ -7,121 +7,152 @@ import axios from 'axios';
 
 @Pipe({ name: 'ImgPipe' })
 export class ImgPipe implements PipeTransform {
-  transform(value: any): any {
-    let ps;
-    if (value) {
-      ps = JSON.parse(value)
+    transform(value: any): any {
+        let ps;
+        if (value) {
+            ps = JSON.parse(value)
+        }
+        // console.log(ps)
+        return ps
     }
-    // console.log(ps)
-    return ps
-  }
 }
 
 @Pipe({ name: 'HouseTypePipe' })
 export class HouseTypePipe implements PipeTransform {
-  transform(items: any): any {
-    let ar, arr;
-    let room = '', hall = '', kitchen = '', bathroom = '';
-    if (items) {
-      ar = items.split('-')
-      arr = ar.map(function (a) {
-        return parseInt(a)
-      })
-      if (arr[0]) {
-        room = arr[0] + '室'
-      }
-      if (arr[1]) {
-        hall = arr[1] + '厅'
-      }
-      if (arr[2]) {
-        kitchen = arr[2] + '厨'
-      }
-      if (arr[3]) {
-        bathroom = arr[3] + '卫'
-      }
+    transform(items: any): any {
+        let ar, arr;
+        let room = '', hall = '', kitchen = '', bathroom = '';
+        if (items) {
+            ar = items.split('-')
+            arr = ar.map(function (a) {
+                return parseInt(a)
+            })
+            if (arr[0]) {
+                room = arr[0] + '室'
+            }
+            if (arr[1]) {
+                hall = arr[1] + '厅'
+            }
+            if (arr[2]) {
+                kitchen = arr[2] + '厨'
+            }
+            if (arr[3]) {
+                bathroom = arr[3] + '卫'
+            }
+        }
+        return room + hall + kitchen + bathroom
     }
-    return room + hall + kitchen + bathroom
-  }
 }
 
 
 @Component({
-  selector: 'nested-com',
-  template: `<div>
+    selector: 'nested-com',
+    template: `<div>
      <h2>from nested-com | {{title}}</h2>
      <span (click)='onClick()'>Click me please!</span>  
   </div>`
 })
 export class NestedCom {
-  @Input() title: any;
-  @Output() notify: EventEmitter<any> = new EventEmitter<any>();
-  constructor() {
-    console.log('Hello NestedCom Provider');
-  }
-  ngOnInit() {
-    console.log('nested-com init');
-  }
-  onClick() {
-    this.notify.emit('Click from nested component');
-  }
+    @Input() title: any;
+    @Output() notify: EventEmitter<any> = new EventEmitter<any>();
+    constructor() {
+        console.log('Hello NestedCom Provider');
+    }
+    ngOnInit() {
+        console.log('nested-com init');
+    }
+    onClick() {
+        this.notify.emit('Click from nested component');
+    }
 }
 
 
 @Component({
-  selector: 'house',
-  templateUrl: '_house.html'
+    selector: 'house',
+    templateUrl: '_house.html'
 })
 export class House {
-  @Input() h: any;
-  @Input() hId: any;
-  @Output() notify: EventEmitter<any> = new EventEmitter<any>();
-  constructor() {
-    // console.log('Hello nested content Provider');
-  }
-  img:any
-  ngOnInit() {
-    // console.log('nested content init');
-    let vm = this
-    if (vm.h){
-       let imgs = JSON.parse(vm.h.images)
-       if (imgs.length){
-         vm.img = imgs[0]
-       } else {
-         vm.img = ''
-       }
+    @Input() h: any;
+    @Input() hId: any;
+    @Output() notify: EventEmitter<any> = new EventEmitter<any>();
+    constructor() {
+        // console.log('Hello nested content Provider');
     }
-    if (vm.hId){
-      let url = '/api/housing/houses/' + vm.hId
-      axios
-        .get(url)
-        .then(function (res) {
-          console.log(res)
-          vm.h = res.data
-            if (vm.h){
-              let imgs = JSON.parse(vm.h.images)
-              if (imgs.length){
+    img: any
+    ngOnInit() {
+        // console.log('nested content init');
+        let vm = this
+        if (vm.h) {
+            let imgs = JSON.parse(vm.h.images)
+            if (imgs.length) {
                 vm.img = imgs[0]
-              } else {
+            } else {
                 vm.img = ''
-              }
             }
-        })
+        }
+        if (vm.hId) {
+            let url = '/api/housing/houses/' + vm.hId
+            axios
+                .get(url)
+                .then(function (res) {
+                    console.log(res.data.id)
+                    vm.h = res.data
+                    if (vm.h) {
+                        let imgs = JSON.parse(vm.h.images)
+                        if (imgs.length) {
+                            vm.img = imgs[0]
+                        } else {
+                            vm.img = ''
+                        }
+                    }
+                })
 
+        }
     }
-  }
-  //   onClick(){
-  //     this.notify.emit('Click from nested content');
-  //   }
+    //   onClick(){
+    //     this.notify.emit('Click from nested content');
+    //   }
 }
 
 
 @Injectable()
 export class TestService {
-  public message: any = "I'm a testservice message";
-  constructor(public http: Http) {
-    console.log('Hello TestService Provider');
-  }
-  test() {
-    console.log('this is from test-service provider')
-  }
+    public message: any = "I'm a testservice message";
+    constructor(public http: Http) {
+        console.log('Hello TestService Provider');
+    }
+    test() {
+        console.log('this is from test-service provider')
+    }
+}
+
+
+
+
+@Component({
+    selector: 'distribution',
+    template:`<span class='telnumber'>{{distributionRole?.name}}&nbsp;{{distributionRole?.mobile}}&nbsp;&nbsp;</span>
+              <span class='phone'><ion-icon ios="ios-call-outline"></ion-icon></span>
+            `
+})
+export class Distribution {
+    distributionRole: any;
+    @Input() r: any;
+    @Output() notify: EventEmitter<any> = new EventEmitter<any>();
+    constructor() {
+        console.log('Hello');
+    }
+    ngOnInit() {
+        let vm = this
+        let url = '/api/account/employees/' + vm.r
+        axios
+            .get(url)
+            .then(function (res) {
+                console.log(res)
+                vm.distributionRole = res.data
+            })
+    }
+    onClick() {
+        this.notify.emit('Click from nested content');
+    }
 }
