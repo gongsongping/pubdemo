e3483ffd7c185baa493d6f976e9ce550b732b390
binh <binh@binhs-MacBook-Pro.local>
2017-04-06 17:06:35 +0800

    datevisitpay预约看房

--- public2/src/pages/housedetails/housedetails.ts
+++ public2/src/pages/housedetails/housedetails.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Districtdetails } from '../districtdetails/districtdetails';
+import { Visitrecord } from '../visitrecord/visitrecord';
+import { Login } from '../login/login';
 
 import axios from 'axios';
 declare var BMap: any;
@@ -17,7 +19,6 @@ declare var BMap: any;
 })
 export class Housedetails {
 
-    userInfo: any;
     house: any;
     isAction = false;
     subdistricts:any;
@@ -42,6 +43,9 @@ export class Housedetails {
         }
         map.enableScrollWheelZoom(true);
     }
+
+    userInfo: any;
+    tokens: any;
     ionViewWillEnter(){
         let vm = this;
         let url = 'api/housing/subdistricts/' + vm.house.subdistrict.id
@@ -49,6 +53,8 @@ export class Housedetails {
             .then(function (res) {
                 vm.subdistricts = res.data;
             })
+        this.userInfo = JSON.parse(localStorage.getItem('userInfo'))    
+        this.tokens = JSON.parse(localStorage.getItem('tokens'))    
     }
     ionViewDidLoad() {
         console.log('----housedetails----- Page did load');
@@ -60,6 +66,123 @@ export class Housedetails {
     action () {
         this.isAction = !this.isAction
     }
+
+    //预约看房
+    loginOrStartBuyRecommend (){
+       let vm = this
+       if (vm.tokens){
+           vm.startBuyRecommend()
+       } else {
+           vm.navCtrl.push(Login)
+       }
+    }
+    
+    buy_recommend_id
+    mission_id
+    mission
+    startBuyRecommend () {
+        // api/activiti/runtime/process-instances
+        let vm = this
+        let bs64 = window.btoa(vm.userInfo.username + ':' + vm.tokens.access_token)
+        axios({
+            method: 'get',
+            headers: { "Authorization": "Basic " + bs64 },
+            url: '/api/activiti/repository/process-definitions?latest=true&key=buy_recommend'
+        }).then(function successCallback(res) {
+                vm.buy_recommend_id = res.data.data[0].id
+                axios({
+                    method: 'post',
+                    url: '/api/activiti/runtime/process-instances',
+                    headers: { "Authorization": "Basic " + bs64 },
+                    data: {
+                        "processDefinitionId": vm.buy_recommend_id,
+                        "variables": [{
+                            "name": "creator_id",
+                            "type": "long",
+                            "value": vm.userInfo.id
+                        }, {
+                            "name": "region_id",
+                            "value": vm.house.regionId//vm.choosedRegion.id
+                        }, {
+                            "name": "customer_name",
+                            "value": vm.userInfo.username //$window.localStorage.user_id
+                        }, {
+                            "name": "customer_mobile",
+                            "value": vm.userInfo.mobile
+                        }]
+                    }
+                }).then(function successCallback(res) {
+                        // alert('提交成功')
+                        // vm.navCtrl.pop()
+                        vm.navCtrl.push(Visitrecord)                       
+                        // vm.mission_id = res.data.value
+                        // axios({
+                        //     method: 'get',
+                        //     url: '/api/mission/missions/' + vm.mission_id
+                        // }).then(function successCallback(res) {
+                        //         vm.mission = res.data
+                        //         if (vm.mission.watchHouseFee == 0) {
+                        //             alert('预约成功,本次看房免费,请确认订单!')
+                        //             // vm.knowmodal.hide()
+                        //             // vm.goVisitrecord()
+                        //             vm.navCtrl.push(Visitrecord)
+                        //         } else {
+                        //             // vm.paymodal.show();
+                        //         }
+                        //     })
+                    }).catch(function errorCallback() {
+                        alert('服务器错误')                
+                    })
+            }).catch(function() {
+                alert('服务器错误')                
+            })
+
+    }
+  
+    //pay alipay
+    // $ionicModal.fromTemplateUrl('templates/_datevisitpay.html', {
+    //     scope: vm,
+    //     animation: 'slide-in-up'
+    // }).then(function (modal) {
+    //     vm.paymodal = modal;
+    // });
+    // vm.$on('$destroy', function () {
+    //     vm.paymodal.remove();
+    // });
+    paySuccess () {
+        let vm = this
+        // vm.knowmodal.hide()
+        // vm.paymodal.hide()
+        // vm.goVisitrecord()
+    }
+    payCancel () {
+        // vm.knowmodal.hide()
+        // vm.paymodal.hide()
+    }
+    openUrl (url) {
+        // console.log('inapp')
+        window.open(url, '_system')
+    }
+
+    alipay () {
+        let vm = this
+        //userAgent 0 mobileapp 1 web
+        let data = `missionId=${vm.mission_id}&userId=${vm.userInfo.id}&orderNumber=${vm.mission.serialNumber}&userAgent=0`
+        axios({
+            method: 'post',
+            url: '/api/payment/alipay',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: data
+        }).then(function successCallback(res) {
+                vm.openUrl(res.data)
+            }).catch(function() {
+                alert('服务器错误')                
+            })
+    }
+
+
 }
 
 
