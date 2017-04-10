d83ec66dde975b0dc7ea009a7ff44af631ab36bb
binh <binh@binhs-MacBook-Pro.local>
2017-04-10 09:58:50 +0800

    newweek

--- staff2/src/pages/tododetails/tododetails.ts
+++ staff2/src/pages/tododetails/tododetails.ts
@@ -358,7 +358,7 @@ export class Tododetails {
         targetHeight: 700,
         destinationType: this.camera.DestinationType.DATA_URL,
         encodingType: this.camera.EncodingType.JPEG,
-        sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+        sourceType:this.camera.PictureSourceType.PHOTOLIBRARY
         // mediaType: Camera.MediaType.PHOTOLIBRARY
       }
 
